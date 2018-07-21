ad2cd8635a66ebd240dd379cc4bed832382223d7
Cosmin Iulian <35537933+Dedwen@users.noreply.github.com>
2018-07-21 13:48:08 +0300

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,57 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.TextField;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class Controller implements Initializable {
+
+    @FXML
+    private TextField addressBar;
+    @FXML
+    private WebView web;
+    @FXML
+    private Button GO;
+    @FXML
+    private ProgressIndicator progress;
+
+    private String htLink = "http://";
+    private String adrsLink;
+    private WebEngine engine;
+
+
+
+    @FXML
+    public void go(ActionEvent event) { // actiunea butonului "go"
+        progress.setProgress(-1);
+        adrsLink = addressBar.getText();
+        engine.load(htLink + adrsLink);
+    }
+
+
+    @FXML
+    public void goEnter(ActionEvent event) { // actiunea butonului "Enter" in text
+        progress.setProgress(-1);
+        adrsLink = addressBar.getText();
+        engine.load(htLink + adrsLink);
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) { // initializarea cu site-ul "google"
+        progress.setProgress(-1);
+        engine = web.getEngine();
+        engine.load(htLink + "www.google.com");
+    }
+
+}
+
+
